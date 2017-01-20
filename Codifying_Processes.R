981a620d2d90ba2f72cb5221ed780edbbf4aa897
chaitanya shah <chaitanya shah>
2017-01-20 09:50:23 +0800

    New commit

---
+++ Codifying_Processes.R
@@ -0,0 +1,8 @@
+# Define goal
+# Get data
+# Codify the process
+# Reuse
+# Replicate
+
+# Basically -> Any company can deploy tools since the process has been codified.
+# It will be the starting point of exploration
